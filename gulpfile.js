2272ba65a9baae09cf6eee2407c4285fb56de313
Aaron John-Baptiste <gravityapple@gmail.com>
2015-05-27 14:09:14 +0100

    added gulp and some tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,59 @@
 var gulp = require('gulp'); 
+var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+
+var onError = function(err) {
+    console.log(err);
+};
+
+gulp.task('css', function () {
+    gulp.src('style.css')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(autoprefixer('last 2 versions', 'ie8', 'ie9'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css/dist'))
+        .pipe(notify({ message: "css done"} ));
+});
 
 gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(gulp.dest('css'));
+    gulp.src('sass/*.scss')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(sass())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('js', function () {
+    gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .on('error', notify.onError({ message: 'JS hint fail'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/dist'));
 });
 
 gulp.task('browser-sync', function() {
-    browserSync.init(["./*.css", "js/*.js"], {
-        proxy: "http://gravityapple.dev",
-        open: false
+    browserSync.init(["style.css", "js/*.js"], {
+        proxy: "http://gravityapple.dev"
     });
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
+gulp.task('default', function () {
+    gulp.run("browser-sync");
+    gulp.run("sass");
+    gulp.run("css");
+    gulp.run("js");
     gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("style.css", ['css']);
+    gulp.watch("js/*.js", ['js']);
 });
